aca0f594dd8b825a22ea86d012c0ae10fa8a6bb7
unknown <andreasmrdja16@gmail.com>
2020-10-21 23:46:33 +0200

    Added sass support. Aligned saved images.

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -1,18 +1,36 @@
 import { ref, onMounted, watch } from 'vue';
 import { Plugins, CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from '@capacitor/core';
 
+// Reactive array for storing photos
+const photos = ref<Photo[]>([]);
+
 export function usePhotoGallery() {
     const { Camera } = Plugins;
 
     const takePhoto = async () => {
+        // Get taken photo
         const cameraPhoto = await Camera.getPhoto({
             resultType: CameraResultType.Uri,
             source: CameraSource.Camera,
             quality: 100
         });
+        
+        // Save taken photo to the array
+        const fileName = new Date().getTime() + '.jpeg';
+        const savedFileImage = {
+            filepath: fileName,
+            webviewPath: cameraPhoto.webPath
+        };
+        photos.value = [savedFileImage, ...photos.value];
     };
 
     return {
+        photos,
         takePhoto
     };
-};
+}
+
+export interface Photo {
+    filepath: string;
+    webviewPath?: string;
+}
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -6,6 +6,15 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <!-- Photos -->
+      <ion-grid>
+        <ion-row>
+          <ion-col class="ion-column" size="6" :key="photo" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      <!-- Camera Button -->
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="takePhoto()">
           <ion-icon :icon="camera"></ion-icon>
@@ -17,19 +26,26 @@
 
 <script lang="ts">
 import { camera, trash, close } from 'ionicons/icons';
-import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import { usePhotoGallery } from '@/composables/usePhotoGallery';
+import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { usePhotoGallery, Photo } from '@/composables/usePhotoGallery';
 
 export default  {
   name: 'Tab2',
-  components: { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent },
+  components: { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg },
   setup() {
-    const { takePhoto } = usePhotoGallery();
+    const { takePhoto, photos } = usePhotoGallery();
 
     return {
+      photos,
       takePhoto,
       camera, trash, close
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.ion-column {
+  margin: 0 auto;
+}
+</style>
